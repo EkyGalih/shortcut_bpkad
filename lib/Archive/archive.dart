0125a9af426a8b3f25a185c1e63e21bca5cab19a
Eky Galih <ekkygalih8@gmail.com>
2023-08-01 12:14:24 +0800

    scroll paginate archive

--- lib/Archive/archive.dart
+++ lib/Archive/archive.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:instabpkad/api/archive_api.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:instabpkad/model/archive/archive_model.dart';
+// import 'package:instabpkad/model/archive/archive_model.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const Archive());
@@ -16,96 +21,127 @@ class Archive extends StatefulWidget {
 }
 
 class _ArchiveState extends State<Archive> {
-  late Future<List<ArchiveModel>> daftarArchive;
+  final scrollController = ScrollController();
+  bool isLoadingMore = false;
+  List archive = [];
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
-    daftarArchive = ArchiveService().getArchive();
+    scrollController.addListener(_scrollListener);
+    fetchArchive();
+  }
+
+  Future<void> fetchArchive() async {
+    final url = 'https://bpkad.ntbprov.go.id/api/kip?page=$page';
+    print(url);
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body)['data']['data'] as List;
+      setState(() {
+        archive = archive + json;
+      });
+    } else {
+      print('Unexpected response');
+    }
+  }
+
+  Future<void> _scrollListener() async {
+    if (isLoadingMore) return;
+    if (scrollController.position.pixels ==
+        scrollController.position.maxScrollExtent) {
+      setState(() {
+        isLoadingMore = true;
+      });
+      page = page + 1;
+      await fetchArchive();
+      setState(() {
+        isLoadingMore = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<dynamic>>(
-        future: daftarArchive,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            var archive = snapshot.data;
-            return RefreshIndicator(
-              onRefresh: () async {
-                await Future.delayed(
-                  const Duration(seconds: 1),
-                );
-                archive = ArchiveService().getArchive();
-                setState(() {});
-              },
-              color: Colors.white,
-              backgroundColor: Colors.blueAccent,
-              child: ListView.builder(
-                padding: const EdgeInsets.all(10),
-                itemCount: archive?.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: SizedBox(
-                          width: 292,
-                          child: ListTile(
-                            leading: const Image(
-                              image: AssetImage('assets/images/archive.png'),
-                              height: 35,
-                              width: 35,
-                            ),
-                            title: Text(archive?[index].nama),
-                            subtitle: Text(
-                              "Informasi ${archive?[index].jenis} (${archive?[index].tahun})"
-                                  .toString(),
-                            ),
-                            onTap: () {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text("${archive?[index].nama}"),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: IconButton(
-                          onPressed: () async {
-                            final String baseUrl = archive?[index].files;
-                            final url = Uri.parse(baseUrl);
-                            if (await canLaunchUrl(url)) {
-                              await launchUrl(
-                                url,
-                                mode: LaunchMode.externalApplication,
-                              );
-                            }
-                          },
-                          icon: const Icon(Icons.download),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(
+            const Duration(seconds: 1),
+          );
+          archive = archive;
+          setState(() {});
         },
+        child: ListView.builder(
+          padding: const EdgeInsets.all(10),
+          controller: scrollController,
+          itemCount: isLoadingMore ? archive.length + 1 : archive.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index < archive.length) {
+              final berita = archive[index];
+              final nama = berita['nama_informasi'];
+              final jenis = berita['jenis_informasi'];
+              final tahun = berita['tahun'];
+              final files = berita['files'];
+              return Row(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: SizedBox(
+                      width: 292,
+                      child: ListTile(
+                        leading: const Image(
+                          image: AssetImage('assets/images/archive.png'),
+                          height: 35,
+                          width: 35,
+                        ),
+                        title: Text(nama),
+                        subtitle: Text(
+                          "Informasi $jenis ($tahun)".toString(),
+                        ),
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(nama),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      onPressed: () async {
+                        final String baseUrl = files;
+                        final url = Uri.parse(baseUrl);
+                        if (await canLaunchUrl(url)) {
+                          await launchUrl(
+                            url,
+                            mode: LaunchMode.externalApplication,
+                          );
+                        }
+                      },
+                      icon: const Icon(Icons.download),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     Navigator.of(context).pushNamed('/add_archive');
-      //   },
-      //   backgroundColor: Colors.blue,
-      //   child: const Icon(Icons.add),
-      // ),
     );
+    // floatingActionButton: FloatingActionButton(
+    //   onPressed: () {
+    //     Navigator.of(context).pushNamed('/add_archive');
+    //   },
+    //   backgroundColor: Colors.blue,
+    //   child: const Icon(Icons.add),
+    // ),
   }
 }
